5f9a7ab0fee48d6535b03ebaada4b0aa25da98d8
Lucas Gabriel <gabif.johann@gmail.com>
2023-10-31 00:59:22 -0300

    add: font size, font weight, font family, line height, radius and space token

--- packages/tokens/src/index.ts
+++ packages/tokens/src/index.ts
@@ -1,1 +1,11 @@
 export * from './colors';
+
+export * from './radii';
+
+export * from './space';
+
+export * from './fonts';
+export * from './font-size';
+export * from './font-weights';
+
+export * from './line-heights';
